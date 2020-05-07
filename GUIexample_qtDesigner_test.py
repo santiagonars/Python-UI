2258f4a3bf8baac1addb6afe21f06d921a04f352
Santiago Norena <san.norena@gmail.com>
2020-05-07 16:32:14 -0400

    Qt Designer
    Create code .ui file created from Qt Designer

---
+++ GUIexample_qtDesigner_test.py
@@ -0,0 +1,65 @@
+# To generate this code, had to type the line below on the command line 
+#    in the same directory as the qt designer file (.ui):
+#
+#            =>   $ pyuic5 -x test.ui -o GUIexample_qtDesigner_test.py
+#
+# -----------------------------------------------------------------------------------
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(718, 563)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.button1 = QtWidgets.QPushButton(self.centralwidget)
+        self.button1.setGeometry(QtCore.QRect(230, 350, 281, 101))
+        self.button1.setObjectName("button1")
+        self.label1 = QtWidgets.QLabel(self.centralwidget)
+        self.label1.setGeometry(QtCore.QRect(270, 90, 201, 81))
+        self.label1.setObjectName("label1")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 718, 22))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionHello = QtWidgets.QAction(MainWindow)
+        self.actionHello.setObjectName("actionHello")
+        self.menuFile.addAction(self.actionHello)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.button1.setText(_translate("MainWindow", "Press Me!"))
+        self.label1.setText(_translate("MainWindow", "Hello my name is Santiago"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.actionHello.setText(_translate("MainWindow", "Hello"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
