502d78bc5482dfa7ee8f9ef0c8dff16fdf62c7f3
Santiago Norena <san.norena@gmail.com>
2020-05-14 12:44:24 -0400

    ComboBox example
    - ComboBox example
    - Created XOR logic when pressing a button
    - Example generated from test2.ui

---
+++ GUIexample_qtDesigner_test2.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test2.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(717, 581)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.comboX = QtWidgets.QComboBox(self.centralwidget)
+        self.comboX.setGeometry(QtCore.QRect(100, 120, 211, 91))
+        self.comboX.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.comboX.setObjectName("comboX")
+        self.comboX.addItem("")
+        self.comboX.addItem("")
+        self.comboY = QtWidgets.QComboBox(self.centralwidget)
+        self.comboY.setGeometry(QtCore.QRect(420, 120, 211, 91))
+        self.comboY.setEditable(False)
+        self.comboY.setObjectName("comboY")
+        self.comboY.addItem("")
+        self.comboY.addItem("")
+        self.submit = QtWidgets.QPushButton(self.centralwidget)
+        self.submit.setGeometry(QtCore.QRect(260, 330, 211, 91))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.submit.setFont(font)
+        self.submit.setObjectName("submit")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(300, 230, 131, 51))
+        font = QtGui.QFont()
+        font.setPointSize(24)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 717, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        # # ----------------------Example to Add / change item index-------------------------
+        # # Add items to the comboBox
+        # self.comboX.addItem("Hello")
+        # # Change default item from code
+        # # 1.) if you put as first argument a value that is not present, it will return -1
+        # # 2.) 2nd argument returns index of where item is 
+        # index = self.comboX.findText("Hello", QtCore.Qt.MatchFixedString)
+        # self.comboX.setCurrentIndex(index) # sets the default index to show up first in box
+        # # ---------------------------------------------------------------------------------
+
+        self.submit.clicked.connect(self.pressed)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.comboX.setItemText(0, _translate("MainWindow", "0"))
+        self.comboX.setItemText(1, _translate("MainWindow", "1"))
+        self.comboY.setItemText(0, _translate("MainWindow", "0"))
+        self.comboY.setItemText(1, _translate("MainWindow", "1"))
+        self.submit.setText(_translate("MainWindow", "SUBMIT"))
+        self.label.setText(_translate("MainWindow", "X XOR Y ="))
+
+    def pressed(self):
+        x = int(self.comboX.currentText())
+        y = int(self.comboY.currentText())
+        xor = (x and not y) or (not x and y)
+        if xor == True:
+            xor = 1
+        else:
+            xor = 0
+        self.label.setText("X XOR Y = " + str(xor))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
